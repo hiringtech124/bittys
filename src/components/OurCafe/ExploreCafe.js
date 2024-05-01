540974cc633e2da07b99ce309a4317a542e49341
Faizan Ansari <hussainfaizan1379@gmail.com>
2024-05-01 17:25:17 +0530

    done about page

--- src/components/OurCafe/ExploreCafe.js
+++ src/components/OurCafe/ExploreCafe.js
@@ -23,28 +23,28 @@ function ExploreCafe() {
         </div>
         <div className='flex justify-center max-lg:grid max-lg:grid-cols-3 max-lg:gap-4 max-md:grid-cols-2 max-md:gap-4 max-sm:grid-cols-1 max-sm:gap-4
         max-md:ml-[50px] max-sm:ml-[30px] max-lg:pb-[30px]'>
-          <div className=' chefS w-[310px] h-[400px] mr-[50px] flex items-center pt-[190px] max-lg:w-[250px] max-lg:h-[300px] max-lg:pt-[100px]' >
+          <div className=' chefS w-[310px] h-[400px] mr-[50px] rounded-lg flex items-center pt-[190px] max-lg:w-[250px] max-lg:h-[300px] max-lg:pt-[100px]' >
             <div className=' flex flex-col items-center'>
               <h1 className='font-serif font-bold text-[32px] pb-5 tracking-wide max-lg:text-[25px] text-[#fff]'>Chef&apos;s Special</h1>
               <p className='pb-5 pl-5 max-lg:text-[13px] text-[#fff]'>Lorem ipsum Lorem ipsum dolor Lorem ipsum Lorem ipsum dolor sit</p>
               <a className='text-[red] text-[20px] font-bold max-lg:text-[16px]'>Explore More</a>
             </div>
           </div>
-          <div className='italian w-[310px] h-[400px] mr-[50px] flex items-center pt-[190px] max-lg:w-[250px] max-lg:h-[300px] max-lg:pt-[100px]' >
+          <div className='italian w-[310px] h-[400px] mr-[50px] rounded-lg flex items-center pt-[190px] max-lg:w-[250px] max-lg:h-[300px] max-lg:pt-[100px]' >
             <div className='flex flex-col items-center'>
               <h1 className='font-serif font-bold text-[32px] pb-5 tracking-wide max-lg:text-[25px] text-[#fff]'>Italian</h1>
               <p className='pb-5 pl-5 max-lg:text-[13px] text-[#fff]'>Lorem ipsum Lorem ipsum dolor sit Lorem ipsum Lorem ipsum dolor sit</p>
               <a className='text-[red] text-[20px] font-bold max-lg:text-[16px]'>Explore More</a>
             </div>
           </div>
-          <div className=' baverage w-[310px] h-[400px] mr-[50px] flex items-center pt-[190px] max-lg:w-[250px] max-lg:h-[300px] max-lg:pt-[100px]' >
+          <div className=' baverage w-[310px] h-[400px] mr-[50px] rounded-lg flex items-center pt-[190px] max-lg:w-[250px] max-lg:h-[300px] max-lg:pt-[100px]' >
             <div className=' flex flex-col items-center'>
               <h1 className='font-serif font-bold text-[32px] pb-5 tracking-wide max-lg:text-[25px] text-[#fff]'>Baverages</h1>
               <p className='pb-5 pl-5 max-lg:text-[13px] text-[#fff]'>Lorem ipsum Lorem ipsum dolor sit Lorem ipsum Lorem ipsum dolor sit</p>
               <a className='text-[red] text-[20px] font-bold max-lg:text-[16px]'>Explore More</a>
             </div>
           </div>
-          <div className='sIndian w-[310px] h-[400px] flex items-center pt-[190px] max-lg:w-[250px] max-lg:h-[300px] max-lg:pt-[100px]'  >
+          <div className='sIndian w-[310px] h-[400px] flex items-center rounded-lg pt-[190px] max-lg:w-[250px] max-lg:h-[300px] max-lg:pt-[100px]'  >
             <div className=' flex flex-col items-center'>
               <h1 className='font-serif font-bold text-[32px] pb-5 tracking-wide max-lg:text-[25px] text-[#fff]'>South Indian</h1>
               <p className='pb-5 pl-5 max-lg:text-[13px] text-[#fff]'>Lorem ipsum Lorem ipsum dolor sit Lorem ipsum Lorem ipsum dolor sit</p>
