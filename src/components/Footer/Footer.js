f7f150cd9b3f0ab5f5ac09fcaadbc25f34d00b1f
Faizan Ansari <hussainfaizan1379@gmail.com>
2024-05-08 17:03:59 +0530

    push till 08 may

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <>
       <div className='bg-[#F9EDE1] bg-cover w-full flex flex-col items-center justify-center'>
-        <div className='w-full flex justify-between max-md:flex-wrap px-[100px] max-xl:px-[60px] max-lg:px-[40px] max-sm:px-[10px] py-[50px] max-sm:py-[10px]'>
+        <div className='w-full flex justify-between max-md:flex-wrap max-[640px]:justify-center max-[640px]:gap-x-[200px] max-[490px]:gap-x-[100px] max-[360px]:gap-x-[50px] px-[100px] max-xl:px-[60px] max-lg:px-[40px] max-sm:px-[10px] py-[50px] max-sm:py-[10px]'>
           <div className='h-[150px] max-sm:h-[200px] mt-[100px] max-xl:mt-[50px] max-md:mt-[20px] max-xl:w-[400px] max-lg:w-[300px] max-sm:w-full max-lg:mt-[50px] max-sm:ml-[10px] max-md:mt-[0px] max-sm:w-[280px]'>
             <h1 className='font-serif font-bold text-[30px] pb-2 max-sm:w-[200px] '>News Letter</h1>
             <div className='pb-5'>
@@ -23,7 +23,7 @@ function Footer() {
               <Link href='/about'><li>About Us</li></Link>
             </ul>
           </div>
-          <div className='w-[350px] max-xl:w-[300px] max-lg:w-[200px] max-sm:w-[150px] mt-[50px] flex flex-col items-end'>
+          <div className='w-[350px] max-xl:w-[300px] max-lg:w-[200px] max-sm:w-[150px] max-sm:mt-[30px] mt-[50px] flex flex-col items-end'>
             <h1 className='font-bold text-[20px] max-lg:text-[18px] max-sm:text-[15px]'>CONTACT US</h1>
             <p className='text-end mt-[10px] max-lg:text-[14px] max-sm:text-[11px]'>Plot No 123, Scheme no 78 Part 2 in Front of Hotel Grand Ten Vijay Nagar Indore, MP, District - Indore 445533</p>
             <p className='max-sm:text-[11px]'>Call Us: +91 91794 44179</p>
@@ -33,7 +33,7 @@ function Footer() {
         <div className='w-full'>
           <div className='border-t-2 border-gray-400 h-[10px] w-full'></div>
           <div className='h-[70px] flex justify-center items-center'>
-            <p className='font-bold text-[20px] max-md:text-[18px] max-sm:text-[14px]'>Copyright ©2024 . All rights reserved.</p>
+            <p className='font-bold text-[20px] max-md:text-[18px] text-center max-sm:text-[14px]'> &quot;Copyright © 2024 All Rights Reserved by HIRING TECH&quot;</p>
           </div>
         </div>
 
