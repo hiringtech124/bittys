aaa978d7452cb533e31212d623acfa9bfd4e8dae
Faizan Ansari <hussainfaizan1379@gmail.com>
2024-05-11 18:12:27 +0530

    PUSH TILL 11 MAY

--- src/components/Menu/menuPage/baverage/Baverage.js
+++ src/components/Menu/menuPage/baverage/Baverage.js
@@ -11,9 +11,9 @@ function Baverage() {
     <>
       <div className='h-full w-full bg-[#F5F9D6] flex flex-col items-center px-[20px]'>
         <h1 className='flex justify-start w-[70%] font-serif font-bold text-[#4D3029] text-[64px] max-xl:text-[54px] max-lg:text-[44px] max-md:text-[34px] max-[450px]:text-[22px] '>Baverage</h1>
-        <div className='grid grid-cols-3 max-md:grid-cols-2 max-[450px]:grid-cols-1 gap-x-[80px] max-xl:gap-x-[30px] max-lg:gap-x-[20px] gap-y-[100px] max-lg:gap-y-[50px] w-[100%] pt-[100px] max-xl:pt-[70px] max-md:pt-[50px]'>
+          <div className='grid grid-cols-3 max-md:grid-cols-2 max-[450px]:grid-cols-1 gap-x-[80px] max-xl:gap-x-[30px] max-lg:gap-x-[20px] gap-y-[100px] max-lg:gap-y-[50px] w-[100%] pt-[100px] max-xl:pt-[70px] max-md:pt-[50px]'>
           <div className='flex flex-col items-center justify-center border border-[#8D4242] w-[220px] h-[260px] max-xl:w-[170px] max-xl:h-[190px] max-lg:w-[120px] max-lg:h-[140px]  relative'>
-            <Image className="w-[150px] h-[150px] max-xl:w-[100px] max-xl:h-[100px] max-lg:w-[70px] max-lg:h-[70px] absolute top-[-35%] max-xl:top-[-30%] " src={img1} alt="Baverage"/>
+            <Image className="w-[150px] h-[150px] max-xl:w-[100px] max-xl:h-[100px] max-lg:w-[70px] max-lg:h-[70px] max-xl:top-[-30%] absolute top-[-35%]  " src={img1} alt="Baverage"/>
             <h1 className='font-serif font-bold text-center text-[24px] max-xl:text-[20px] max-lg:text-[16px] max-md:text-[14px] tracking-wide mt-[30px]'>Ginger Tea</h1>
             <h2 className='font-serif font-bold text-center text-[32px] max-xl:text-[28px] max-lg:text-[24px] max-md:text-[18px] tracking-wide'>15/-</h2>
           </div>
--- src/components/Menu/menuPage/others/Other.js
+++ src/components/Menu/menuPage/others/Other.js
@@ -9,38 +9,38 @@ import Image from 'next/image'
 function Other() {
   return (
     <>
-      <div className='h-full w-full bg-[#F5F9D6] flex flex-col items-center'>
-        <h1 className='flex justify-start w-[70%] font-serif font-bold text-[#4D3029] text-[64px] '>Other Offerings</h1>
-        <div className='grid grid-cols-3 gap-x-[100px] gap-y-[100px] w-[80%] pt-[100px]'>
-          <div className='flex flex-col items-center justify-center border border-[#8D4242] w-[220px] h-[260px] relative'>
-            <Image className="w-[150px] h-[150px] absolute top-[-35%] " src={img1} alt="Baverage" />
-            <h1 className='font-serif font-bold text-center text-[24px] tracking-wide mt-[30px]'>Khamand</h1>
-            <h2 className='font-serif font-bold text-center text-[32px] tracking-wide'>30/-</h2>
+      <div className='h-full w-full bg-[#F5F9D6] flex flex-col items-center px-[20px]'>
+        <h1 className='flex justify-start w-[70%] font-serif font-bold text-[#4D3029] text-[64px] max-xl:text-[54px] max-lg:text-[44px] max-md:text-[34px] max-[450px]:text-[22px]'>Other Offerings</h1>
+        <div className='grid grid-cols-3 max-md:grid-cols-2 max-[450px]:grid-cols-1 gap-x-[80px] max-xl:gap-x-[30px] max-lg:gap-x-[20px] gap-y-[100px] max-lg:gap-y-[50px] w-[100%] pt-[100px] max-xl:pt-[70px] max-md:pt-[50px]'>
+          <div className='flex flex-col items-center justify-center border border-[#8D4242] w-[220px] h-[260px] max-xl:w-[170px] max-xl:h-[190px] max-lg:w-[120px] max-lg:h-[140px] relative'>
+            <Image className="w-[150px] h-[150px] max-xl:w-[100px] max-xl:h-[100px] max-lg:w-[70px] max-lg:h-[70px] max-xl:top-[-30%] absolute top-[-35%] " src={img1} alt="Baverage" />
+            <h1 className='font-serif font-bold text-center text-[24px] max-xl:text-[20px] max-lg:text-[16px] max-md:text-[14px] tracking-wide mt-[30px]'>Khamand</h1>
+            <h2 className='font-serif font-bold text-center text-[32px] max-xl:text-[28px] max-lg:text-[24px] max-md:text-[18px] tracking-wide'>30/-</h2>
           </div>
-          <div className='flex flex-col items-center justify-center border border-[#8D4242] w-[220px] h-[260px] relative'>
-            <Image className="w-[150px] h-[150px] absolute top-[-35%]" src={img2} alt="Baverage" />
-            <h1 className='font-serif font-bold text-center text-[24px] tracking-wide mt-[30px]'>Bhel Puri</h1>
-            <h2 className='font-serif font-bold text-center text-[32px] tracking-wide'>30/-</h2>
+          <div className='flex flex-col items-center justify-center border border-[#8D4242] w-[220px] h-[260px] max-xl:w-[170px] max-xl:h-[190px] max-lg:w-[120px] max-lg:h-[140px] relative'>
+            <Image className="w-[150px] h-[150px] max-xl:w-[100px] max-xl:h-[100px] max-lg:w-[70px] max-lg:h-[70px] max-xl:top-[-30%] absolute top-[-35%]" src={img2} alt="Baverage" />
+            <h1 className='font-serif font-bold text-center text-[24px] max-xl:text-[20px] max-lg:text-[16px] max-md:text-[14px] tracking-wide mt-[30px]'>Bhel Puri</h1>
+            <h2 className='font-serif font-bold text-center text-[32px] max-xl:text-[28px] max-lg:text-[24px] max-md:text-[18px] tracking-wide'>30/-</h2>
           </div>
-          <div className='flex flex-col items-center justify-center border border-[#8D4242] w-[220px] h-[260px] relative'>
-            <Image className="w-[150px] h-[150px] absolute top-[-35%]" src={img3} alt="Baverage" />
-            <h1 className='font-serif font-bold text-center text-[24px] tracking-wide mt-[30px]'>Pav Bhaji</h1>
-            <h2 className='font-serif font-bold text-center text-[32px] tracking-wide'>80/-</h2>
+          <div className='flex flex-col items-center justify-center border border-[#8D4242] w-[220px] h-[260px] max-xl:w-[170px] max-xl:h-[190px] max-lg:w-[120px] max-lg:h-[140px] relative'>
+            <Image className="w-[150px] h-[150px] max-xl:w-[100px] max-xl:h-[100px] max-lg:w-[70px] max-lg:h-[70px] max-xl:top-[-30%] absolute top-[-35%]" src={img3} alt="Baverage" />
+            <h1 className='font-serif font-bold text-center text-[24px] max-xl:text-[20px] max-lg:text-[16px] max-md:text-[14px] tracking-wide mt-[30px]'>Pav Bhaji</h1>
+            <h2 className='font-serif font-bold text-center text-[32px] max-xl:text-[28px] max-lg:text-[24px] max-md:text-[18px] tracking-wide'>80/-</h2>
           </div>
-          <div className='flex flex-col items-center justify-center border border-[#8D4242] w-[220px] h-[260px] relative'>
-            <Image className="w-[150px] h-[150px] absolute top-[-35%]" src={img4} alt="Baverage" />
-            <h1 className='font-serif font-bold text-center text-[24px] tracking-wide mt-[30px]'>French fries</h1>
-            <h2 className='font-serif font-bold text-center text-[32px] tracking-wide'>80/-</h2>
+          <div className='flex flex-col items-center justify-center border border-[#8D4242] w-[220px] h-[260px] max-xl:w-[170px] max-xl:h-[190px] max-lg:w-[120px] max-lg:h-[140px] relative'>
+            <Image className="w-[150px] h-[150px] max-xl:w-[100px] max-xl:h-[100px] max-lg:w-[70px] max-lg:h-[70px] max-xl:top-[-30%] absolute top-[-35%]" src={img4} alt="Baverage" />
+            <h1 className='font-serif font-bold text-center text-[24px] max-xl:text-[20px] max-lg:text-[16px] max-md:text-[14px] tracking-wide mt-[30px]'>French fries</h1>
+            <h2 className='font-serif font-bold text-center text-[32px] max-xl:text-[28px] max-lg:text-[24px] max-md:text-[18px] tracking-wide'>80/-</h2>
           </div>
-          <div className='flex flex-col items-center justify-center border border-[#8D4242] w-[220px] h-[260px] relative'>
-            <Image className="w-[150px] h-[150px] absolute top-[-35%]" src={img5} alt="Baverage" />
-            <h1 className='font-serif font-bold text-center text-[24px] tracking-wide mt-[30px]'>Rasgulla</h1>
-            <h2 className='font-serif font-bold text-center text-[32px] tracking-wide'>30/-</h2>
+          <div className='flex flex-col items-center justify-center border border-[#8D4242] w-[220px] h-[260px] max-xl:w-[170px] max-xl:h-[190px] max-lg:w-[120px] max-lg:h-[140px] relative'>
+            <Image className="w-[150px] h-[150px] max-xl:w-[100px] max-xl:h-[100px] max-lg:w-[70px] max-lg:h-[70px] max-xl:top-[-30%] absolute top-[-35%]" src={img5} alt="Baverage" />
+            <h1 className='font-serif font-bold text-center text-[24px] max-xl:text-[20px] max-lg:text-[16px] max-md:text-[14px] tracking-wide mt-[30px]'>Rasgulla</h1>
+            <h2 className='font-serif font-bold text-center text-[32px] max-xl:text-[28px] max-lg:text-[24px] max-md:text-[18px] tracking-wide'>30/-</h2>
           </div>
-          <div className='flex flex-col items-center justify-center border border-[#8D4242] w-[220px] h-[260px] relative  '>
-            <Image className="w-[150px] h-[150px] absolute top-[-35%]" src={img6} alt="Baverage" />
-            <h1 className='font-serif font-bold text-center text-[24px] tracking-wide mt-[30px]'>Gulab jamun</h1>
-            <h2 className='font-serif font-bold text-center text-[32px] tracking-wide'>30/-</h2>
+          <div className='flex flex-col items-center justify-center border border-[#8D4242] w-[220px] h-[260px] max-xl:w-[170px] max-xl:h-[190px] max-lg:w-[120px] max-lg:h-[140px] relative  '>
+            <Image className="w-[150px] h-[150px] max-xl:w-[100px] max-xl:h-[100px] max-lg:w-[70px] max-lg:h-[70px] max-xl:top-[-30%] absolute top-[-35%]" src={img6} alt="Baverage" />
+            <h1 className='font-serif font-bold text-center text-[24px] max-xl:text-[20px] max-lg:text-[16px] max-md:text-[14px] tracking-wide mt-[30px]'>Gulab jamun</h1>
+            <h2 className='font-serif font-bold text-center text-[32px] max-xl:text-[28px] max-lg:text-[24px] max-md:text-[18px] tracking-wide'>30/-</h2>
           </div>
         </div>
       </div>
