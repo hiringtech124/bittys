a211cd5d2dc0fdee882381d764c5bcd6f18ba675
Faizan Ansari <hussainfaizan1379@gmail.com>
2024-05-21 14:41:36 +0530

    push till 21 may

--- src/components/cafeapp/cafeher.jsx
+++ src/components/cafeapp/cafeher.jsx
@@ -1,25 +1,25 @@
+
 "use client"
 import React from 'react'
-import { useState , useEffect } from "react";
+import { useState, useEffect } from "react";
+import { useRouter } from 'next/navigation';
 import menuData from "../../menu.json";
-import Image from 'next/image';
-
 import './css/cafeher.css'
+
 
 const Cafeher = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [filteredItems, setFilteredItems] = useState([]);
-  const [selectedItem, setSelectedItem] = useState(null);
+
+  const router = useRouter();
 
   useEffect(() => {
-    
     if (searchTerm === "") {
       setFilteredItems([]);
       return;
     }
 
     const items = menuData.categories.flatMap(category => category.items);
-    console.log(items)
     const filtered = items.filter(item => 
       item.itemName && item.itemName.toLowerCase().includes(searchTerm.toLowerCase())
     );
@@ -38,21 +38,17 @@ const Cafeher = () => {
       item.itemName && item.itemName.toLowerCase().includes(searchTerm.toLowerCase())
     );
 
-    console.log("Filtered Items:", filtered); // Debugging line
-
     setFilteredItems(filtered);
   };
-  console.log(filteredItems)
 
-  const handleSelectItem = item => {
-    setSelectedItem(item);
+  const handleSelectItem = item => {  
     setFilteredItems([]);
     setSearchTerm("");
+    router.push("/Menu") 
   };
 
-  const closePopup = () => {
-    setSelectedItem(null);
-  };
+ 
+
   return (
     <>
       <div className='cafehead relative text-[#fff] h-full w-full flex flex-col justify-center items-center pt-[10px] pb-[180px] max-sm:pb-[80px]'>
@@ -83,23 +79,12 @@ const Cafeher = () => {
                 <h1 className="font-serif px-[10px] text-[24px] max-xl:text-[22px] max-lg:text-[18px] max-md:text-[16px] max-sm:text-[10px]">No such food items</h1>
               </div>
             )
-        )}
+          )}
         </div>
-        {selectedItem && (
-          <div className="popup text-black">
-            <div className="popup-content w-[400px] max-lg:w-[250px] max-sm:w-[150px] flex flex-col items-center relative">
-              <span className="close" onClick={closePopup}>
-                &times;
-              </span>
-              <Image src={selectedItem.img}  alt={selectedItem.item} />
-              <h2 className='text-[32px] max-lg:text-[28px] max-md:text-[22px] max-sm:text-[14px] font-serif font-bold w-[400px] max-lg:w-[200px] max-sm:w-[140px]  pb-[20px] max-sm:pb-[5px]'>{selectedItem.itemName}</h2>
-              <p className='text-[26px] max-md:text-[22px] max-sm:text-[18px] font-serif font-semibold'>{selectedItem.price}</p>
-            </div>
-          </div>
-        )}
       </div>
     </>
   )
 }
 
 export default Cafeher
+
